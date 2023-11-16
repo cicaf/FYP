2ff6471119480144c92faa001bcbcae1cb3f7c9e
kenkomu <komukenneth216@gmail.com>
2023-11-16 10:19:51 +0300

    completed the contract

--- contract/app/src/models/property.rs
+++ contract/app/src/models/property.rs
@@ -1,19 +1,22 @@
-use near_sdk::store::Vector;
-use near_sdk::{near_bindgen, env, AccountId};
+use near_sdk::{near_bindgen, AccountId, env};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::Vector;
+use near_sdk::serde::{Deserialize, Serialize};
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub enum PropertyType {
     House,
     Commercial,
     Apartment,
     Lot,
     Garage,
-
 }
+
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub enum PropertyStatus {
     Sale,
     Rent,
@@ -21,17 +24,19 @@ pub enum PropertyStatus {
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct PropertyContactInformation {
-    pub user_id:i32,
-    pub  name: String,
-    pub  username: String,
-    pub  email: String,
-    pub  phone: String,
+    pub user_id: i32,
+    pub name: String,
+    pub username: String,
+    pub email: String,
+    pub phone: String,
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct PropertyLocation {
     pub address: String,
     pub city: String,
@@ -42,31 +47,59 @@ pub struct PropertyLocation {
 }
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct PropertyMedia {
-    pub  id: i32,
+    pub id: i32,
     pub url: String,
 }
 
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct PropertyRating {
-    pub rating:f32,
-    pub  user_id:i32
-}
+
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
 pub struct Property {
     pub id: i32,
-    pub owner:AccountId,
-    pub is_available:bool,
+    pub owner: AccountId,
+    pub is_available: bool,
     pub title: String,
     pub description: String,
     pub status: PropertyStatus,
     pub price: i32,
     pub area: i32,
-    pub contact_information : PropertyContactInformation,
+    pub contact_information: PropertyContactInformation,
     pub location: PropertyLocation,
-    pub rating: Vector<PropertyRating>
+
+}
+
+impl Default for Property {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            owner: env::current_account_id(),
+            is_available: false,
+            title: "".to_string(),
+            description: "".to_string(),
+            status: PropertyStatus::NotAvailable,
+            price: 0,
+            area: 0,
+
+            contact_information: PropertyContactInformation {
+                name: "".to_string(),
+                email: "".to_string(),
+                phone: "".to_string(),
+                user_id: 0,
+                username: "".to_string(),
+            },
+            location: PropertyLocation {
+                address: "".to_string(),
+                city: "".to_string(),
+                lat: 0.0,
+                long: 0.0,
+                county: "".to_string(),
+                state: "".to_string(),
+            },
+        }
+    }
 }
